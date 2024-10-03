98f8ad846ea0888a65111c23b8d3d0df91569bb2
JoJo-214 <161169932+JoJo-214@users.noreply.github.com>
2024-10-03 13:55:33 +0100

    Add files via upload

---
+++ calculateArea(width, height).js
@@ -0,0 +1,10 @@
+function calculateArea(width, height) {
+    return width * height;
+}
+
+// Example usage:
+const width = 5;
+const height = 10;
+const area = calculateArea(width, height);
+
+console.log(`The area of the rectangle is: ${area}`);
---
+++ function cube(num) {.js
@@ -0,0 +1,8 @@
+function cube(num) {
+    return num * num * num;
+}
+
+// Example usage
+const number = 3;
+const result = cube(number);
+console.log(`The cube of ${number} is ${result}.`);
